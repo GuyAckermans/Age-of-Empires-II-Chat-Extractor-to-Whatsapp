c81f70751fc4cbe659ffff35bc92331a6c1d466b
Guy Ackermans <guy.ackermans92@gmail.com>
2025-09-27 22:43:33 +0200

    rename

--- start_script.bat
+++ start_script.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "C:\Users\guyac\Documents\Age-of-Empires-II-Chat-Extractor to Whatsapp"
-"C:\Users\guyac\AppData\Local\Programs\Python\Python311\pythonw.exe" "extract chat.py"
+"C:\Users\guyac\AppData\Local\Programs\Python\Python311\pythonw.exe" "extract-chat.py"
